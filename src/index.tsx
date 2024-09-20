7d4446bd72a086237d02756ac4d9f748697b3eb1
AintripinKWX <yelisey_toyota@mail.ru>
2024-09-20 16:38:35 +0300

    React and SCSS now stitched in

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import {render} from "react-dom";
+import {Counter} from "./components/counter";
 
 render(
-    <div>Shiiiiieeet</div>,
+    <div>Shiiiiieeet
+    <Counter />
+        </div>,
     document.getElementById("root")
 )
