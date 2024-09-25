1aaa6225dc39a0ed6a5ea7c4e0c32befcf1e1be1
AintripinKWX <yelisey_toyota@mail.ru>
2024-09-25 17:59:05 +0300

    yet another commit

--- build_02/src/App.tsx
+++ build_02/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {Suspense} from 'react';
 import Counter from "./components/Counter";
-import './index.scss';
+import './styles/index.scss';
 import {Routes, Route, Link} from 'react-router-dom';
 import AboutPage from "./pages/AboutPage/AboutPage";
 import MainPage from "./pages/MainPage/MainPage";
