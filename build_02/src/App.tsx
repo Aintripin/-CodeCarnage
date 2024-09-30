6845fffbf2b02e1a7a71829eb51a271dab21d906
Aintripin <yelisey_toyota@mail.ru>
2024-09-29 17:10:17 -0700

    classNames

--- build_02/src/App.tsx
+++ build_02/src/App.tsx
@@ -1,14 +1,11 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import Counter from "./components/Counter";
 import './styles/index.scss';
-import {Routes, Route, Link} from 'react-router-dom';
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import {Link, Route, Routes} from 'react-router-dom';
 import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
 import MainPageAsync from "./pages/MainPage/MainPage.async";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
-
+import {classNames} from "./helpers/classNames/classNames";
 
 
 const App = () => {
@@ -16,8 +13,9 @@ const App = () => {
     const {theme, toggleTheme} = useTheme()
 
 
+
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", {}, [theme])}>
 
             <button onClick={() => toggleTheme()}>Change Theme</button>
 
