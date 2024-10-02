189256594df3becfd17376eacbd0b8a7ef753c97
Aintripin <yelisey_toyota@mail.ru>
2024-10-01 22:36:18 -0700

    App Router

--- build_02/src/app/App.tsx
+++ build_02/src/app/App.tsx
@@ -3,8 +3,7 @@ import './styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -24,12 +23,7 @@ const App = () => {
             <br/>
             <Link to={`/white_people`}>wipepo</Link>
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={`/about`} element={<AboutPage />} />
-                    <Route path={`/`} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
 
         </div>
     );
