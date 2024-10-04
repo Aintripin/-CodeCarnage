31b302e0f930d15ed189058e9ff580b73c43c2a7
AintripinKWX <yelisey_toyota@mail.ru>
2024-10-04 17:59:06 +0300

    Sidebar

--- build_03/src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ build_03/src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,21 +1,17 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
 import cls from './Sidebar.module.scss';
-import {useState} from "react";
+import { useState } from "react";
 
 interface SidebarProps {
     className?: string;
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
-
-    const [collapsed, setCollaped] = useState(false);
-
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
 
     return (
-        // <div className={classNames (cls.Sidebar, {[cls.collapsed], [className])}>
-        <div className={classNames(cls.Sidebar, {[cls.collapsed]}, [className])}>
-
-
+        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+            {/* Sidebar content goes here */}
         </div>
     );
 };
