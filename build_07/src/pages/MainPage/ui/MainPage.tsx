daac3fdd498bdcc244abf00c36afa75077c00a43
AintripinKWX <yelisey_toyota@mail.ru>
2024-10-28 17:40:43 +0300

    running on fumes now, not sure what's good anymore

--- build_07/src/pages/MainPage/ui/MainPage.tsx
+++ build_07/src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import {Loader} from "shared/ui/Loader/Loader";
 
 const MainPage = () => {
     const { t } = useTranslation();
