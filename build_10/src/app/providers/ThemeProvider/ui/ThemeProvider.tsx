846104ae99bb143dd0371b03140feb29d6ec22e2
AintripinKWX <yelisey_toyota@mail.ru>
2024-11-05 12:04:15 +0300

    I guess I kinda fixed it?

--- build_10/src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ build_10/src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,16 +1,9 @@
-// ThemeProvider.tsx
-import React, {
-    FC, useMemo, useState, ReactNode,
-} from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
-
-interface ThemeProviderProps {
-    children: ReactNode;
-}
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+const ThemeProvider: FC = ({ children }) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
 
     const defaultProps = useMemo(() => ({
@@ -24,3 +17,5 @@ export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
         </ThemeContext.Provider>
     );
 };
+
+export default ThemeProvider;
--- build_10/webpack.config.ts
+++ build_10/webpack.config.ts
@@ -2,7 +2,6 @@ import webpack from 'webpack';
 import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
@@ -14,79 +13,15 @@ export default (env: BuildEnv) => {
 
     const mode = env.mode || 'development';
     const PORT = env.port || 3000;
+
     const isDev = mode === 'development';
 
-    const config: webpack.Configuration = {
+    const config: webpack.Configuration = buildWebpackConfig({
         mode,
-        entry: paths.entry,
-        output: {
-            filename: '[name].[contenthash].js',
-            path: paths.build,
-            clean: true,
-            publicPath: '/',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|tsx)$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: {
-                                    auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                                    localIdentName: isDev
-                                        ? '[path][name]__[local]--[hash:base64:5]'
-                                        : '[hash:base64:8]',
-                                },
-                            },
-                        },
-                        'sass-loader',
-                    ],
-                },
-                {
-                    test: /\.svg$/,
-                    use: ['@svgr/webpack'],
-                },
-            ],
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                __IS_DEV__: JSON.stringify(isDev),
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash:8].css',
-                chunkFilename: 'css/[name].[contenthash:8].css',
-            }),
-        ],
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js', '.jsx'],
-            preferAbsolute: true,
-            modules: [paths.src, 'node_modules'],
-            mainFiles: ['index'],
-            alias: {
-                '@': paths.src,
-                'app': path.resolve(__dirname, 'src/app'),
-                'shared': path.resolve(__dirname, 'src/shared'),
-                'entities': path.resolve(__dirname, 'src/entities'),
-                'features': path.resolve(__dirname, 'src/features'),
-                'widgets': path.resolve(__dirname, 'src/widgets'),
-                'pages': path.resolve(__dirname, 'src/pages'),
-            },
-        },
-        devServer: {
-            port: PORT,
-            open: true,
-            historyApiFallback: true,
-            hot: true,
-        },
-    };
+        paths,
+        isDev,
+        port: PORT,
+    });
 
     return config;
 };
