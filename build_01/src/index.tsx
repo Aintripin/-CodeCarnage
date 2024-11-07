62361a99e9e7834fb3d603b04070edd00030b80b
AintripinKWX <yelisey_toyota@mail.ru>
2024-11-07 12:38:15 +0300

    initial commit

--- build_01/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import {render} from "react-dom";
-import {Counter} from "./components/counter";
-
-render(
-    <div>Shiiiiieeet
-    <Counter />
-        </div>,
-    document.getElementById("root")
-)
--- build_06/src/shared/lib/classNames/classNames.test.ts
+++
@@ -1,7 +0,0 @@
-import { classNames } from "./classNames";
-
-describe("classNames", () => {
-    test("test", () => {
-        expect(true).toBe(true);
-    })
-})
